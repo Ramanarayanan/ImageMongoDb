183b117e1dce520f3e9cf8f9bcb8cc24ff497c13
Ramanarayanan <Ramanarayanank.in@gmail.com>
2022-08-29 20:03:08 +0530

    Change for mongodb

---
+++ Models/ImageModel.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageMongoDb.Models
+{
+    public class ImageModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id
+        { get; set; }
+
+        public string ImageName
+        { get; set; }
+    }
+}
